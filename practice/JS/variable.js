7d7917c4bb3768049516c8dd46affe756590e621
naknak <9naknak8@gmail.com>
2023-04-09 20:15:35 +0900

    practice 0409

---
+++ practice/JS/variable.js
@@ -0,0 +1,15 @@
+var student = {
+    grade: 1,
+    school: "lion school"
+};
+
+console.log(student.grade)
+
+var name3 = Symbol("naknak");
+var name4 = Symbol("naknak");
+
+
+console.log(name3 == name4);
+console.log();
+
+// === 타입이 같은지 아닌지를 확인하는 연산자
